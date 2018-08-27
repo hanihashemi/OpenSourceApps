b5013b49b47473f6236b6a1828f1bc198ac06d05
Hani Hashemi <jhanihashemi@gmail.com>
2018-08-27 19:24:09 +0300

    Make parcelable

--- app/src/main/java/ir/opensourceapps/android/model/Owner.kt
+++ app/src/main/java/ir/opensourceapps/android/model/Owner.kt
@@ -1,11 +1,14 @@
 package ir.opensourceapps.android.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Owner(
         val id: Long,
         @field:SerializedName("avatar_url")
         val avatarUrl: String,
         val url: String,
         val type: String
-)
+) : Parcelable
--- app/src/main/java/ir/opensourceapps/android/model/Repo.kt
+++ app/src/main/java/ir/opensourceapps/android/model/Repo.kt
@@ -1,7 +1,10 @@
 package ir.opensourceapps.android.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Repo(
         val id: Long,
         @field:SerializedName("full_name")
@@ -11,11 +14,11 @@ data class Repo(
         val homepage: String,
         @field:SerializedName("html_url")
         val htmlUrl: String,
-        val language: String,
+        val language: String?,
         @field:SerializedName("open_issues")
         val openIssues: Int,
         val forks: Int,
         @field:SerializedName("stargazers_count")
         val stars: Int,
         val owner: Owner
-)
+) : Parcelable
