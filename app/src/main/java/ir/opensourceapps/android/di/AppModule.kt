2ba3050dd2079da872798448955973ee2f35f892
Hani Hashemi <jhanihashemi@gmail.com>
2018-08-27 09:26:49 +0300

    Add BrowseViewModel and RepoRepository to AppModule

--- app/src/main/java/ir/opensourceapps/android/di/AppModule.kt
+++ app/src/main/java/ir/opensourceapps/android/di/AppModule.kt
@@ -2,7 +2,10 @@ package ir.opensourceapps.android.di
 
 import ir.opensourceapps.android.data.network.RetrofitUtil
 import ir.opensourceapps.android.data.network.api.RepoApi
+import ir.opensourceapps.android.data.repository.RepoRepository
+import ir.opensourceapps.android.ui.browse.BrowseViewModel
 import ir.opensourceapps.android.util.AppExecutors
+import org.koin.android.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.Module
 import org.koin.dsl.module.module
 
@@ -10,9 +13,15 @@ val appModule: Module = module {
 
     single { AppExecutors() }
 
+    //ViewModel
+    viewModel<BrowseViewModel>()
+
     //Network
     single { RetrofitUtil.create() }
 
     //API
     single<RepoApi>()
+
+    //Repository
+    single<RepoRepository>()
 }
