65fbb361aa54000df56426043745c455c58bbf02
Hani Hashemi <jhanihashemi@gmail.com>
2018-08-27 08:50:03 +0300

    Add AppExecutors and add it to AppModule

--- app/src/main/java/ir/opensourceapps/android/di/AppModule.kt
+++ app/src/main/java/ir/opensourceapps/android/di/AppModule.kt
@@ -2,10 +2,13 @@ package ir.opensourceapps.android.di
 
 import ir.opensourceapps.android.data.network.RetrofitUtil
 import ir.opensourceapps.android.data.network.api.RepoApi
+import ir.opensourceapps.android.util.AppExecutors
 import org.koin.dsl.module.Module
 import org.koin.dsl.module.module
 
 val appModule: Module = module {
+
+    single { AppExecutors() }
 
     //Network
     single { RetrofitUtil.create() }
