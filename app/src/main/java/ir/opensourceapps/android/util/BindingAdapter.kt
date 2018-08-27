1455e3871265a4af7dcd25cb5b87552486400a8b
Hani Hashemi <jhanihashemi@gmail.com>
2018-08-27 18:32:07 +0300

    Add glide library and its databinding adapter

---
+++ app/src/main/java/ir/opensourceapps/android/util/BindingAdapter.kt
@@ -0,0 +1,12 @@
+package ir.opensourceapps.android.util
+
+import android.databinding.BindingAdapter
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+@BindingAdapter("imageUrl")
+fun setImageUrl(imageView: ImageView, url: String) {
+    Glide.with(imageView.context)
+            .load(url)
+            .into(imageView)
+}
