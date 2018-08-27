12b240253d453ba9202c734f2a62e37b9d7e160e
Hani Hashemi <jhanihashemi@gmail.com>
2018-08-27 08:35:19 +0300

    Add Retrofit to AppModule

--- app/src/main/java/ir/opensourceapps/android/di/AppModule.kt
+++ app/src/main/java/ir/opensourceapps/android/di/AppModule.kt
@@ -1,8 +1,11 @@
 package ir.opensourceapps.android.di
 
+import ir.opensourceapps.android.data.network.RetrofitUtil
 import org.koin.dsl.module.Module
 import org.koin.dsl.module.module
 
 val appModule: Module = module {
 
+    //Network
+    single { RetrofitUtil.create() }
 }
