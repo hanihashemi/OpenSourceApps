09b46e750d77b8af27bc56a42cd8c9e3a5828d49
Hani Hashemi <jhanihashemi@gmail.com>
2018-08-27 08:35:02 +0300

    Add RetrofitUtil and Adapter
    RetrofitUtil can provide Retrofit instance.
    LiveDataCallAdapter will change response to ApiResponse.

---
+++ app/src/main/java/ir/opensourceapps/android/data/network/RetrofitUtil.kt
@@ -0,0 +1,35 @@
+package ir.opensourceapps.android.data.network
+
+import ir.opensourceapps.android.BuildConfig
+import ir.opensourceapps.android.data.adapter.LiveDataCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitUtil {
+    companion object {
+        private const val base_url = "https://api.github.com/"
+
+        fun create(): Retrofit {
+            val client = OkHttpClient().newBuilder()
+
+            if (BuildConfig.DEBUG) {
+                val interceptor = HttpLoggingInterceptor()
+                interceptor.level = HttpLoggingInterceptor.Level.BODY
+                client.addInterceptor(interceptor)
+            }
+
+            return Retrofit.Builder()
+                    .baseUrl(base_url)
+                    .client(client.build())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(LiveDataCallAdapterFactory())
+                    .build()
+        }
+    }
+
+    class ApiProvider(private val retrofit: Retrofit) {
+
+    }
+}
