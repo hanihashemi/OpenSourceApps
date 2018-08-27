57e8413d8ff06c8c580e103d38af7fe1e9da884a
Hani Hashemi <jhanihashemi@gmail.com>
2018-08-27 08:17:50 +0300

    Add koin library (dependency manager)

--- app/src/main/java/ir/opensourceapps/android/App.kt
+++ app/src/main/java/ir/opensourceapps/android/App.kt
@@ -2,14 +2,16 @@ package ir.opensourceapps.android
 
 import android.app.Application
 import android.support.v7.app.AppCompatDelegate
+import ir.opensourceapps.android.di.appModule
 import ir.opensourceapps.android.util.CrashReportingTree
+import org.koin.android.ext.android.startKoin
 import timber.log.Timber
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        startKoin(this, listOf(appModule))
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
-
         initTimber()
     }
 
---
+++ app/src/main/java/ir/opensourceapps/android/di/AppModule.kt
@@ -0,0 +1,8 @@
+package ir.opensourceapps.android.di
+
+import org.koin.dsl.module.Module
+import org.koin.dsl.module.module
+
+val appModule: Module = module {
+
+}
