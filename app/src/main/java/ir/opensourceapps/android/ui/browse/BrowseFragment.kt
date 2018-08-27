7569fa5f23528629a5ce5603d71a42914cfdc85f
Hani Hashemi <jhanihashemi@gmail.com>
2018-08-27 09:42:39 +0300

    Setup recyclerview and send request

--- app/src/main/java/ir/opensourceapps/android/ui/browse/BrowseFragment.kt
+++ app/src/main/java/ir/opensourceapps/android/ui/browse/BrowseFragment.kt
@@ -1,19 +1,31 @@
 package ir.opensourceapps.android.ui.browse
 
+import android.arch.lifecycle.Observer
+import android.support.v7.widget.LinearLayoutManager
 import ir.opensourceapps.android.R
 import ir.opensourceapps.android.base.BaseFragment
+import ir.opensourceapps.android.ui.browse.adapter.RepoAdapter
+import kotlinx.android.synthetic.main.browse_fragment.*
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class BrowseFragment() : BaseFragment() {
-
-    override val layoutResource: Int = R.layout.browse_fragment
-
     companion object {
         fun instance(): BrowseFragment {
             return BrowseFragment()
         }
     }
 
-    override fun customizeUI() {
+    override val layoutResource: Int = R.layout.browse_fragment
+    private val vm: BrowseViewModel by viewModel()
 
+    override fun customizeUI() {
+        rclRepo.layoutManager = LinearLayoutManager(context!!)
+        rclRepo.adapter = RepoAdapter()
+
+        vm.list.observe(this, Observer {
+            (rclRepo.adapter as RepoAdapter).submitList(it)
+        })
+
+        vm.searchIt("")
     }
 }
