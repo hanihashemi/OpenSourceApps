424b1cad382c414d1e0b4561ae88bd8989325335
Hani Hashemi <jhanihashemi@gmail.com>
2018-08-28 06:28:00 +0300

    handle fail network event

--- app/src/main/java/ir/opensourceapps/android/ui/browse/BrowseFragment.kt
+++ app/src/main/java/ir/opensourceapps/android/ui/browse/BrowseFragment.kt
@@ -1,12 +1,15 @@
 package ir.opensourceapps.android.ui.browse
 
 import android.arch.lifecycle.Observer
+import android.support.design.widget.Snackbar
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import ir.opensourceapps.android.R
 import ir.opensourceapps.android.base.Fragment
 import ir.opensourceapps.android.data.network.datasource.NetworkState
+import ir.opensourceapps.android.data.network.datasource.Status
 import ir.opensourceapps.android.model.Repo
 import ir.opensourceapps.android.ui.browse.adapter.RepoAdapter
 import ir.opensourceapps.android.ui.browse.adapter.RepoListener
@@ -14,7 +17,7 @@ import ir.opensourceapps.android.ui.repo.RepoActivity
 import kotlinx.android.synthetic.main.browse_fragment.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class BrowseFragment() : Fragment(), RepoListener {
+class BrowseFragment : Fragment(), RepoListener {
     companion object {
         fun instance(): BrowseFragment {
             return BrowseFragment()
@@ -26,12 +29,30 @@ class BrowseFragment() : Fragment(), RepoListener {
     override fun customizeUI() {
         initSwipeRefresh()
         initRecycler()
+        handleNetworkFail()
+        observeList()
 
+        vm.searchIt("")
+    }
+
+    private fun observeList() {
         vm.list.observe(this, Observer {
             (rclRepo.adapter as RepoAdapter).submitList(it)
         })
+    }
 
-        vm.searchIt("")
+    private fun handleNetworkFail() {
+        vm.networkState.observe(this, Observer {
+            if (it?.status == Status.FAILED) {
+                val snackBar = Snackbar.make(
+                        coordinator,
+                        it.msg.toString(),
+                        Snackbar.LENGTH_INDEFINITE)
+                snackBar.setAction(R.string.action_retry) { _ ->
+                    vm.retry()
+                }.show()
+            }
+        })
     }
 
     private fun initSwipeRefresh() {
@@ -55,7 +76,7 @@ class BrowseFragment() : Fragment(), RepoListener {
         rclRepo.adapter = RepoAdapter(this)
     }
 
-    override fun inflateLayout(inflater: LayoutInflater, container: ViewGroup?) =
+    override fun inflateLayout(inflater: LayoutInflater, container: ViewGroup?): View =
             inflater.inflate(R.layout.browse_fragment, container, false)
 
     override fun onRepoClick(repo: Repo) = RepoActivity.start(context!!, repo)
